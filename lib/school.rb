00ba2ec828686d445f86fbb1d98debde43eb5c03
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-18 20:02:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,19 +2,19 @@ require 'pry'
 
 class School  
   
-  ROSTER = []
+  # ROSTER = []
   
   def initialize(name)
     @name = name
   end 
   
-  def add_student(name, grade)
-    ROSTER << grade
-    binding.pry
-    if !ROSTER[grade].include?(name)
-      ROSTER[grade] << name
-    end
-  end
+  # def add_student(name, grade)
+  #   ROSTER << grade
+  #   binding.pry
+  #   if !ROSTER[grade].include?(name)
+  #     ROSTER[grade] << name
+  #   end
+  # end
 
 end
 
