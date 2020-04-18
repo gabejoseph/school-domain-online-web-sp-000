b1c2a378cb00b3e50f0641fd8738d2b4491efd3b
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-18 21:02:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -23,8 +23,8 @@ class School
   def sort
     @roster.each do |grade, student|
       if @roster[grade][student].sort != @roster[grade][student]
-        @roster[grade][student] = @roster[grade][student].sort
         binding.pry
+        @roster[grade][student] = @roster[grade][student].sort
       end
     end
   end
