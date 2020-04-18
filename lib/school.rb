340704aa261735297d0ae1e522f2a61b5aae5edc
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-18 19:55:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,16 @@
 class School  
   
-  ROSTER = []
+  ROSTER = ""
   
   def initialize(name)
     @name = name
   end 
   
   def add_student(name, grade)
-    
+    ROSTER[grade] = []
+    if !ROSTER[grade].include?(name)
+      ROSTER[grade] << name
+    end
   end
 
 end
