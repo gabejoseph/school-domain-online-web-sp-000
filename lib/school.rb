6d940efc4143e56e526115405fa63ef1c1ca7ab8
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-18 19:49:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,15 +2,8 @@ class School
   
   ROSTER = []
   
-  def initialize(name)
-    @name = name
-  end
-  
-  def add_student
-    # if !ROSTER.include?(roster)
-    ROSTER << name
-  end
-  
-end
+  def school(name)
+  end 
 
+end
 
